499358ec048082f2af0cfe0154104ac915b64244
004321 <141306364+004321@users.noreply.github.com>
2023-08-03 12:35:18 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class subtraction
+{
+    public static void main(String[]args)
+{
+    System.out.println("substraction of two numbers");
+    int a=10;
+    int b=20;
+    int c=a-b;
+    System.out.println("result is "+c);
+}
+}
